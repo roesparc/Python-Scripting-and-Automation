e26cab402c34a0743906e5f84b49034f778713b4
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-10-24 11:21:35 -0600

    ADD: once script finishes, open workbook in excel application

--- Excel Automation/EA2.py
+++ Excel Automation/EA2.py
@@ -18,3 +18,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "EA2.xlsx")
 
 workbook.save(filename=file_path)
+
+xl = Dispatch("Excel.Application")
+xl.Visible = True
+xl.WorkBooks.Open(file_path)
