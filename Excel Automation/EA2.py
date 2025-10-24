58b512e5d914e24dd70695da23acc0f4133c137a
Rodrigo Espinosa <respinosa@sma.com.mx>
2025-10-24 11:05:16 -0600

    ADD: rows from pandas dataframe to excel workbook

--- Excel Automation/EA2.py
+++ Excel Automation/EA2.py
@@ -1,8 +1,20 @@
 import pandas as pd
-from openpyxl import load_workbook
+from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from win32com.client import Dispatch
+import os
 
 data = {"Asset Name": ["Asset 1", "Asset 2"], "Month 1": [15, 30], "Month 2": [5, 35]}
 
 df = pd.DataFrame(data)
+
+workbook = Workbook()
+sheet = workbook.active
+
+for row in dataframe_to_rows(df, index=False, header=True):
+    sheet.append(row)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "EA2.xlsx")
+
+workbook.save(filename=file_path)
