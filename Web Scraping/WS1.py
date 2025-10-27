6f0b318ff46e03e27bb57c3c822383987d65fc39
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-10-27 17:37:52 -0600

    ADD: fix data reading by wrapping response text in StringIO for pandas and print tables

--- Web Scraping/WS1.py
+++ Web Scraping/WS1.py
@@ -1,10 +1,14 @@
 import requests
 import pandas as pd
+from io import StringIO
 
 url = "https://en.wikipedia.org/wiki/Premier_League"
 headers = {"User-Agent": "Mozilla/5.0"}
 
 response = requests.get(url, headers=headers)
-scraper = pd.read_html(response.text)
+scraper = pd.read_html(StringIO(response.text))
 
-print(scraper)
+for i, table in enumerate(scraper):
+    print("---")
+    print(i)
+    print(table)
