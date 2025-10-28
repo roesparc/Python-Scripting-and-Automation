69f1c912884b4793b706f291c231b7ea25e1f810
Rodrigo Espinosa <rodesar95@hotmail.com>
2025-10-27 19:07:03 -0600

    ADD: create CSV file for Premier League DataFrame

--- Web Scraping/WS1.py
+++ Web Scraping/WS1.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from io import StringIO
+import os
 
 url = "https://en.wikipedia.org/wiki/Premier_League"
 headers = {"User-Agent": "Mozilla/5.0"}
@@ -8,7 +9,14 @@ headers = {"User-Agent": "Mozilla/5.0"}
 response = requests.get(url, headers=headers)
 scraper = pd.read_html(StringIO(response.text))
 
-for i, table in enumerate(scraper):
-    print("---")
-    print(i)
-    print(table)
+# for i, table in enumerate(scraper):
+#     print("---")
+#     print(i)
+#     print(table)
+
+df = scraper[1]
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "WS1.csv")
+
+df.to_csv(file_path, index=False)
